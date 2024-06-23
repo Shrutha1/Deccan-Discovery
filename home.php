285e6970bdd2ed29d465e71beee250dc057fcf31
Shrutha1 <139142992+Shrutha1@users.noreply.github.com>
2024-06-23 16:45:26 +0530

    home.php

---
+++ home.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <section class="header">
+  <a href="home.php" class="logo">Deccan Discovery</a>
+  <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+  </nav>
+  <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<section class="home">
+  <div class="swiper home-slider">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" style="background:url(images/footer5.jpg)no-repeat"  >
+        <div class="content">
+           <span>explore, discover, travel</span>
+           <h3>Travel around Karnataka</h3>
+           <a href="package.php" class="btn">Discover more</a>
+        </div>
+      </div>
+      <div class="swiper-slide" style="background:url(images/footer4.jpg)no-repeat" >
+        <div class="content">
+           <span>explore, discover, travel</span>
+           <h3>discover new places</h3>
+           <a href="package.php" class="btn">Discover more</a>
+        </div>
+      </div>
+      <div class="swiper-slide" style="background:url(images/footer3.jpg)no-repeat" >
+        <div class="content">
+           <span>explore, discover, travel</span>
+           <h3>make your tour worthwhile</h3>
+           <a href="package.php" class="btn">Discover more</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+</section>
+
+
+<section class="services">
+  <h1 class="heading-title">our services</h1>
+  <div class="box-container">
+    <div class="box">
+      <img src="images/icon 1.png" alt="" width=40px>
+      <h3><a href="index.html">adventure</a></h3>
+    </div>
+    <div class="box">
+      <img src="images/tour-guide.png" alt="" width=40px>
+      <h3><a href="index.html">tour guide</a></h3>
+    </div>
+    <div class="box">
+      <img src="images/trekking.png" alt=" " width=40px>
+      <h3><a href="index.html">trekking</a></h3>
+    </div>
+    <div class="box">
+      <img src="images/bonfire.png" alt="" width=40px>
+      <h3><a href="index.html">camp fire</a></h3>
+    </div>
+    <div class="box">
+      <img src="images/jeep.png" alt="" width=40px>
+      <h3><a href="index.html">off road</a></h3>
+    </div>
+    <div class="box">
+      <img src="images/tent.png" alt="" width=40px>
+      <h3><a href="index.html">camping</a></h3>
+    </div>
+  </div>
+</section>
+
+
+<section class="home-about">
+  <div class="image">
+    <img src="images/about.jpg" alt="" >
+  </div>
+  <div class="content">
+    <h3>about us</h3>
+    <p>Welcome to Deccan Discovery, your one-stop platform to uncover the magic of Karnataka! We're passionate about helping you explore Karnataka's hidden gems, rich culture, and stunning landscapes. Our website lets you seamlessly book trips across Karnataka, crafting your dream itinerary.</p>
+    <a href="about.php" class="btn">read more</a>
+  </div>
+</section>
+
+<section class="home-packages">
+  <h1 class="heading-title">our packages</h1>
+  <div class="box-container">
+    <div class="box">
+      <div class="image">
+        <img src="images/unchalli.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Jog Falls</h3>
+        <p>Unleash the power of nature at <b style="color: purple;">Jog Falls</b>, Karnataka's crown jewel. Witness four cascades, aptly named Raja, Rani, Roarer, and Rocket, thundering down a dizzying 830 feet. Breathe in the mist-filled air and be mesmerized by the raw beauty of this cascading wonder.</p>
+        <a href="#book.php" class="btn">book now </a>
+      </div>
+    </div>
+
+    <div class="box">
+      <div class="image">
+        <img src="images/honnavara.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Honnavar's backwaters</h3>
+        <p><b style="color: purple;"> Honnavar's backwaters</b> aren't for white-water thrills, but peaceful chills! Glide on calm waters, surrounded by lush islands and vibrant forests. Spot birds, colorful boats, and soak up the tranquility. Perfect for all ages on a relaxing coastal adventure.</p>
+        <a href="#book.php" class="btn">book now </a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/skandagiri.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Skandagiri Hills</h3>
+        <p>Hike up <b style="color: purple;">Skandagiri Hills</b> for breathtaking views and a touch of history.  Challenge yourself with a pre-dawn climb to witness a stunning sunrise paint the sky with vibrant colors and explore the ancient temple perched atop the hills.</p>
+        <a href="#book.php" class="btn">book now </a>
+      </div>
+    </div>
+
+  </div>
+  <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+</section>
+
+<section class="home-offer">
+  <div class="content">
+    <h3>upto 50% off</h3>
+    <p>Unleash your inner explorer and discover the magic of Karnataka at unbeatable prices! Book your dream trip through our website and save up to 50% on select adventures. From trekking Skandagiri Hills to unwinding in Honnavar's backwaters, explore Karnataka without breaking the bank!</p>
+    <a href="book.php" class="btn">book now</a>
+  </div>
+</section>
+
+
+<section class="footer">
+
+<div class="box-container">
+  <div class="box">
+    <h3>quick links</h3>
+  <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+    <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+    <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+    <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+  </div>
+  <div class="box">
+    <h3>extra links</h3>
+  <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+  <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+  <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+  <a href="#"> <i class="fas fa-angle-right"></i> terms of use </a>
+  </div>
+  <div class="box">
+    <h3>contact info</h3>
+  <a href="#"> <i class="fas fa-phone"></i>+123-456-7890</a>
+  <a href="#"> <i class="fas fa-phone"></i>+111-222-333-4444</a>
+  <a href="#"> <i class="fas fa-envelope"></i>niveditag056@gmail.com</a>
+  <a href="#"> <i class="fas fa-map"></i>Bangalore, India-560064</a>
+  </div>
+  <div class="box">
+    <h3>follow us</h3>
+    <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+    <a href="#"> <i class="fab fa-twitter"> </i>twitter </a>
+    <a href="#"> <i class="fab fa-instagram"> </i>instagram</a>
+    <a href="#"> <i class="fab fa-linkedin"> </i>linkedin </a>
+  </div>
+</div>
+<div class="credit">created by <span>Nivedita G</span> | all rights reserved!</div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="home.js"></script>
+</body>
+</html>
