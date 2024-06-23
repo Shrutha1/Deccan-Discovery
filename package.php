d1c1836629e9cab2d66a597a807b43a98a6778ce
Shrutha1 <139142992+Shrutha1@users.noreply.github.com>
2024-06-23 16:45:57 +0530

    Create package.php

---
+++ package.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>package</title>
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <section class="header">
+  <a href="home.php" class="logo">travel.</a>
+  <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+  </nav>
+  <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/package.jpg)no-repeat">
+    <h1>packages</h1>
+</div>
+<section class="packages">
+  <h1 class="heading-title">top destinations</h1>
+  <div class="box-container">
+    <div class="box">
+      <div class="image">
+        <img src="images/package2.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>St. Mary's Islands</h3>
+        <p>Set sail for adventure at <b style="color:#8e44ad;"> St. Mary's Islands, Udupi!</b> Explore unique rock formations sculpted by ancient volcanic lava. Kayak around these geological wonders, or relax on pristine beaches. Unwind amidst the beauty of the Arabian Sea and discover Karnataka's hidden gem.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/honnavara.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Honnavar's Backwaters</h3>
+        <p><b style="color:#8e44ad;">Honnavar's Backwaters </b>Aren't For White-Water Thrills, But Peaceful Chills! Glide On Calm Waters, Surrounded By Lush Islands And Vibrant Forests. Spot Birds, Colorful Boats, And Soak Up The Tranquility. Perfect For All Ages On A Relaxing Coastal Adventure.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package3.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Agastya Lake</h3>
+        <p>Immerse yourself in history at <b style="color:#8e44ad;">Agastya Lake,</b> Badami. This serene lake, named after the legendary sage Agastya, reflects the beauty of the surrounding Badami Caves.  Carved into the red sandstone cliffs, these ancient temples overlook the tranquil waters. Take a boat ride, explore the intricate carvings, and delve into the rich heritage of this captivating destination.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package4.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Netrani Island</h3>
+        <p>Dive into an underwater paradise at <b style="color:#8e44ad;"> Netrani Island,</b> Karnataka's "Pigeon Island"!  Descend into crystal-clear waters teeming with colorful fish, vibrant coral reefs, and even majestic sharks. Explore shipwrecks, encounter playful dolphins, and experience the magic of the underwater world off the Murdeshwar coast.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package5.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Yana caves</h3>
+        <p>Journey to the heart of Karnataka's lush forests and uncover the <b style="color:#8e44ad;"> Yana caves!</b> Explore natural wonders – towering black limestone formations and a sacred Shiva lingam, believed to be self-formed. Hike through vibrant greenery and discover the beauty and mystery hidden within these ancient caves.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package6.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Om Beach</h3>
+        <p>Embrace the serenity and adventure of <b style="color:#8e44ad;">Om Beach!</b> Bask on the golden sands shaped like the sacred "Om" symbol.  Thrill-seekers can take on water sports like surfing and parasailing. Explore nearby Half Moon Beach or trek to viewpoints for breathtaking coastal panoramas. Om Beach offers a perfect blend of relaxation and adventure.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package7.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Kumara Parvatha</h3>
+        <p>Ascend the mighty <b style="color:#8e44ad;">Kumara Parvatha,</b> Karnataka's second-highest peak! Traverse lush forests, conquer challenging inclines, and be rewarded with breathtaking views from the summit. This trek is not for the faint of heart, but the sense of accomplishment and the panoramic vistas make it an unforgettable adventure.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package8.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Bannerghatta National Park</h3>
+        <p>Explore the wild side of Bangalore at <b style="color:#8e44ad;"> Bannerghatta National Park!</b> Witness a diverse range of animals, from majestic tigers and playful monkeys to towering elephants.  This unique park combines a zoo, butterfly park, and safari adventures, offering a fun and educational experience for the whole family.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="images/package9.jpg" alt="">
+      </div>
+      <div class="content">
+        <h3>Sahasrakunda Falls</h3>
+        <p>Unveiling the majesty of Karnataka, <b style="color:#8e44ad;">Sahasrakunda Falls</b> near Belgaum stuns with its multi-tiered descent. Witness the cascading wonder, plunging a total of 310 feet, best admired during the monsoon's vibrant display. Explore scenic viewpoints or take a thrilling coracle boat ride for an unforgettable close-up experience of this natural marvel.</p>
+        <a href="book.php" class="btn">book now</a>
+      </div>
+    </div>
+    
+  </div>
+  <div class="load-more"><span class="btn">load more</span></div>
+</section>
+
+
+
+
+<section class="footer">
+
+<div class="box-container">
+  <div class="box">
+    <h3>quick links</h3>
+  <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+    <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+    <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+    <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+  </div>
+  <div class="box">
+    <h3>extra links</h3>
+  <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+  <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+  <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+  <a href="#"> <i class="fas fa-angle-right"></i> terms of use </a>
+  </div>
+  <div class="box">
+    <h3>contact info</h3>
+  <a href="#"> <i class="fas fa-phone"></i>+123-456-7890</a>
+  <a href="#"> <i class="fas fa-phone"></i>+111-222-333-4444</a>
+  <a href="#"> <i class="fas fa-envelope"></i>niveditag056@gmail.com</a>
+  <a href="#"> <i class="fas fa-map"></i>Bangalore, India-560064</a>
+  </div>
+  <div class="box">
+    <h3>follow us</h3>
+    <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+    <a href="#"> <i class="fab fa-twitter"> </i>twitter </a>
+    <a href="#"> <i class="fab fa-instagram"> </i>instagram</a>
+    <a href="#"> <i class="fab fa-linkedin"> </i>linkedin </a>
+  </div>
+</div>
+<div class="credit">created by <span>Nivedita G</span> | all rights reserved!</div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="home.js"></script>
+</body>
+</html>
